cd379c618134eaee0c14846238e1e830faae36da
TimiMaliki <malikitimi@gmail.com>
2025-08-09 08:28:32 +0100

    reduced the size of text in headers section

--- src/component/Headers.jsx
+++ src/component/Headers.jsx
@@ -5,7 +5,7 @@ const Header = ({intro , title}) => {
         <h4 className="text-lg font-bold flex justify-center text-center ovo-regular mb-3">
         {intro}
         </h4>
-        <h2 className="text-center text-4xl font-medium">{title}</h2>
+        <h2 className="text-center text-xl md:text-4xl font-medium">{title}</h2>
       </div>
         </div>
      );
