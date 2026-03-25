926c3a8f7d38c05425e85e7d86e5adfb7f938dde
TimiMaliki <malikitimi@gmail.com>
2026-03-25 15:47:02 +0100

    updated Bio

--- src/sections/Works.jsx
+++ src/sections/Works.jsx
@@ -1,45 +1,46 @@
-import {FaArrowAltCircleRight } from "react-icons/fa";
+import { FaArrowAltCircleRight } from "react-icons/fa";
 import Buttons from "../component/buttons";
 import Header from "../component/Headers";
 import { Link } from "react-router-dom";
-import hospital from "../img/projects/hospital/Screenshot from 2025-04-23 03-05-54.png"
-import women from "../img/projects/women-fashion/Screenshot from 2025-04-23 03-12-04.png"
-import light from "../img/projects/light/41.png"
+import caped from "../img/projects/Caped/caped1.png";
+import hospital from "../img/projects/hospital/Screenshot from 2025-04-23 03-05-54.png";
+import women from "../img/projects/women-fashion/Screenshot from 2025-04-23 03-12-04.png";
+import light from "../img/projects/light/41.png";
 
 const LatestWorks = () => {
   const projects = [
     {
       title: "Caped – E-commerce Web Application ",
-      description:`Experience the future of retail through a fluid, secure, and hyper-responsive interface designed to put the user first. We’ve stripped away the friction of traditional e-commerce to create a beautifully synced shopping environment that works everywhere you do.`,
-      image:hospital,
+      description: `Experience the future of retail through a fluid, secure, and hyper-responsive interface designed to put the user first. We’ve stripped away the friction of traditional e-commerce to create a beautifully synced shopping environment that works everywhere you do.`,
+      image: caped,
       tags: ["Html", "Css", "Bootstrap"],
       year: "2023",
-      link: "/projects/jobbazar",
+      link: "https://caped-stores.vercel.app/",
     },
     {
       title: "Dental Hospital landing page",
-      description:`a modern dental hospital dedicated to providing exceptional oral care in a comfortable and patient-centered environment.`,
-      image:hospital,
+      description: `a modern dental hospital dedicated to providing exceptional oral care in a comfortable and patient-centered environment.`,
+      image: hospital,
       tags: ["Html", "Css", "Bootstrap"],
       year: "2023",
-      link: "/projects/jobbazar",
+      link: "https://dental-clinic-landing-page-ten.vercel.app/",
     },
     {
       title: "X Fashion",
       description: `We specialize in curated women’s fashion that blends quality, comfort, and sophistication—designed to empower confidence in every step`,
-      image:women,
+      image: women,
       tags: ["Html", "Css", "TailwindCss"],
       year: "2022",
-      link: "/projects/slepp",
+      link: "https://maliki-fashion-ecommerce.vercel.app/",
     },
     {
       title: "LightRun",
-      description:`The ultimate blogging hub for storytellers, trend-watchers, and digital voices. Designed for modern bloggers, our platform makes 
+      description: `The ultimate blogging hub for storytellers, trend-watchers, and digital voices. Designed for modern bloggers, our platform makes 
       it easy to share news, celebrity updates, lifestyle insights, and thought-provoking content with a wide and engaged audience.`,
       image: light,
       tags: ["ReactJs", "TailwindCss"],
       year: "2023",
-      link: "/projects/costa",
+      link: "https://lightrun.vercel.app",
     },
   ];
 
@@ -47,43 +48,54 @@ const LatestWorks = () => {
     <section className="py-20 ovo-regular" id="work">
       <div className="w-full max-w-7xl mx-auto px-1">
         {/* Header */}
-        <Header intro={"Portfolio showcase"} title={"My Work"} id=""/>
+        <Header intro={"Portfolio showcase"} title={"My Work"} id="" />
         {/* Projects Grid */}
         <div className="grid gap-2 md:grid-cols-2 lg:grid-cols-2">
           {projects.map((project, index) => (
-            <div
-              key={index}
-              className="group relative  p-5 rounded-2xl shadow-lg hover:shadow-xl transition-all duration-300 border border-gray-100  cursor-pointer "
-            >
-              {/* Image */}
-              <div className="overflow-hidden rounded-xl mb-5">
-                <img
-                  src={project.image}
-                  alt={project.title}
-                  className="w-full h-48 object-cover rounded-xl transition-transform duration-500 group-hover:scale-110"
-                />
-              </div>
+            <Link to={project.link}>
+              <div
+                key={index}
+                className="group relative  p-5 rounded-2xl shadow-lg hover:shadow-xl transition-all duration-300 border border-gray-100  cursor-pointer "
+              >
+                {/* Image */}
+                <div className="overflow-hidden rounded-xl mb-5">
+                  <img
+                    src={project.image}
+                    alt={project.title}
+                    className="w-full h-48 object-cover rounded-xl transition-transform duration-500 group-hover:scale-110"
+                  />
+                </div>
 
-              {/* Tags */}
-              <div className="flex flex-wrap gap-2 mb-3">
-                {project.tags.map((tag, i) => (
-                  <span key={i} className="text-base font-light">
-                    {tag}
-                  </span>
-                ))}
-              </div>
+                {/* Tags */}
+                <div className="flex flex-wrap gap-2 mb-3">
+                  {project.tags.map((tag, i) => (
+                    <span key={i} className="text-base font-light">
+                      {tag}
+                    </span>
+                  ))}
+                </div>
 
-              {/* Title & Description */}
-              <h3 className="text-md md:text-xl font-light md:font-bold mb-2">{project.title}</h3>
-              <p className=" text-sm mb-4">{project.description}</p>
-            </div>
+                {/* Title & Description */}
+                <h3 className="text-md md:text-xl font-light md:font-bold mb-2">
+                  {project.title}
+                </h3>
+                <p className=" text-sm mb-4">{project.description}</p>
+              </div>
+            </Link>
           ))}
         </div>
 
         <div className="flex justify-center align-middle py-8">
-            <Link to="/projects">
-            <Buttons color={"border shadow-[1.95px_1.95px_2.6px_rgba(0,0,0,0.15)]  hover:bg-red-600 hover:text-white transition-all duration-300"} textColor={""} icons={<FaArrowAltCircleRight  className="w-5 h-5 mt-1 "/>} text={"view more"} />
-            </Link>
+          <Link to="/projects">
+            <Buttons
+              color={
+                "border shadow-[1.95px_1.95px_2.6px_rgba(0,0,0,0.15)]  hover:bg-red-600 hover:text-white transition-all duration-300"
+              }
+              textColor={""}
+              icons={<FaArrowAltCircleRight className="w-5 h-5 mt-1 " />}
+              text={"view more"}
+            />
+          </Link>
         </div>
       </div>
     </section>
